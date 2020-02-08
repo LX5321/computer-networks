214312cc63926b9cf579609e920bd9e06898372b
lx5321 <alexanderwebsite5321@gmail.com>
2020-02-08 18:23:38 +0530

    Removed comments & changed client.c

--- ClientServerC/server.c
+++ ClientServerC/server.c
@@ -12,18 +12,11 @@ void error(char *msg)
 
 int main(int argc, char *argv[])
 {
-    /*
-    sockfd and newsockfd are file descriptors.
-    portno stores the port number on which the server accepts connections.
-    clilen stores the size of the address of the client. 
-    n is the return value for the read() and write() calls
-    */
     int sockfd, newsockfd, portno, clilen, n;
     char buffer[256];
     struct sockaddr_in serv_addr, cli_addr;
     if (argc < 2)
     {
-        // check the port number
         fprintf(stderr, "ERROR, no port provided\n");
         exit(1);
     }
@@ -31,9 +24,9 @@ int main(int argc, char *argv[])
 
     if (sockfd < 0)
     {
-        // socket connection fail.
         error("ERROR opening socket");
     }
+
     bzero((char *)&serv_addr, sizeof(serv_addr));
     portno = atoi(argv[1]);
     serv_addr.sin_family = AF_INET;
@@ -63,7 +56,7 @@ int main(int argc, char *argv[])
             error("ERROR reading from socket");
         }
         printf("Here is the message: %s\n", buffer);
-        n = write(newsockfd, "Thenks.\nRegards Alexander..", 18);
+        n = write(newsockfd, "\nThenks.\nRegards Alexander..", 18);
         if (n < 0)
             error("ERROR writing to socket");
     }
